ac5c22a0b7018a2b77e110915d4e5314925b7841
Adrian Silva Mendez <adsilmen@gmail.com>
2020-04-20 01:15:00 -0500

    Se pulió LEX y se planteó la base de YACC
    Arreglé algunos bugs de LEX y agregué algunos otros tokens como brackets, operadores de matrices y operadores lógicos que no teníamos en los tokens de la primera entrega. Intenté continuar con Yacc sin mucho éxito. Checar comentarios en el documento patito++.py para más información.

--- patito++.py
+++ patito++.py
@@ -1,3 +1,21 @@
+"""
+Todo el lexer funciona igual. Agregué algunos otros elementos como los operadores
+de logica (&& y ||), brackets y los operadores de matrices. Intente seguirle con
+el YACC y cree algo en base a la documentación pero no funciona. En sí ya tengo
+declaradas las reglas para expresión. Cambié un poco las reglas, porque no es
+necesario tener tantas reglas llamadas "termino". En sí, puedes tener todos los
+operadores en un mismo set, y les añades precedencia en la función de precedence
+que se ve más abajo. El problema es que al tratar de parsear un string, la variable
+'p' no contiene nada. Entonces no puedo trabajar con nada que arroje yacc. Si te fijas
+hay un nuevo documento llamado parser.out. Ahí se puede ver todo lo que hace el parser
+Ya es muy tarde y no tuve mucho tiempo de observarlo para ver si encontraba algo.
+Si puedes dale una repasada a todo lo que codié del YACC y fijate si puedes arreglar
+el bug. Nuevamente el bug es que las variables P tienen valores en [0] y [1] como None.
+"""
+
+
+
+
 import ply.lex as lex
 import ply.yacc as yacc
 import sys
@@ -30,10 +48,17 @@ tokens = [
     'NUMBER',
     'PLUS',
     'MINUS',
-    'TIMES',
+    'MULTIPLY',
     'DIVIDE',
     'LPAREN',
     'RPAREN',
+    'RELOP',
+    'ASSIGN',
+    'LOGIC',
+    'OBRACKET',
+    'CBRACKET',
+    'SEMICOLON',
+    'OPMATRIZ'
 ]
 
 
@@ -42,10 +67,8 @@ Digito:  [0-9]
 Digitos:  Digito+
 Numero:  Digitos(\. Digitos)?
 Letra: [A-Za-z]
-Caracteres: Caracter+
-Relop: < | > | <= | >= | <> | ==
 Tipo: (Int | Float | Char)
-Ws: ( Blank | Nab | Newline)+
+
 """
 
 def t_COMENTARIO(t):
@@ -53,13 +76,33 @@ def t_COMENTARIO(t):
     t.type = 'COMENTARIO'
     return t
 
+def t_LOGIC(t):
+    r'\&\& | \|\|'
+    t.type = 'LOGIC'
+    return t
+
+def t_RELOP(t):
+    r'<= | >= | <> | < | > | =='
+    t.type = 'RELOP'
+    return t
+
+def t_ASSIGN(t):
+    r'='
+    t.type = 'ASSIGN'
+    return t
+
 def t_IGNORE(t):
-    r'\t '
-    t.lexer.skip(1)
+    r'\ '
+    #t.lexer.skip(1)
 
 def t_NEWLINE(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+
+def t_OPMATRIZ(t):
+    r'\$ | \¡ | \?'
+    t.type = 'OPMATRIZ'
+    return t
 
 def t_PLUS(t):
     r'\+'
@@ -71,14 +114,29 @@ def t_MINUS(t):
     t.type = 'MINUS'
     return t
 
-def t_TIMES(t):
+def t_MULTIPLY(t):
     r'\*'
-    t.type = 'TIMES'
+    t.type = 'MULTIPLY'
     return t
 
 def t_DIVIDE(t):
     r'\/'
     t.type = 'DIVIDE'
+    return t
+
+def t_OBRACKET(t):
+    r'\{'
+    t.type = 'OBRACKET'
+    return t
+
+def t_CBRACKET(t):
+    r'\}'
+    t.type = 'CBRACKET'
+    return t
+
+def t_SEMICOLON(t):
+    r'\;'
+    t.type = 'SEMICOLON'
     return t
 
 def t_LPAREN(t):
@@ -208,10 +266,7 @@ def t_error(t):
 lexer = lex.lex()
 
 lexer.input('''
-PROGRAMA prog
-
-VAR x = 3 + 4
-
+A + B - (C*D/E?)
 ''')
 
 while True:
@@ -219,3 +274,79 @@ while True:
     if not tok:
         break
     print(tok)
+
+print("\n\n")
+
+precedence = (
+    ('left','PLUS','MINUS'),
+    ('left','MULTIPLY','DIVIDE'),
+    ('left','OPMATRIZ')
+)
+
+def p_expresion(p):
+    '''
+    expresion : expresion LOGIC expresion
+              | termino RELOP expresion
+              | termino
+    '''
+    print(p)
+
+    if(len(p) == 1):
+        p[0] = p[1]
+    else:
+        p[0] = (p[2],p[1],p[3])
+
+
+
+    #elif p[2] == "&&" || p[2] == "||":
+    #    if p[2] == "&&":
+    #        p[0] = (p[1] && p[3])
+    #    else:
+    #        p[0] = (p[1] || p[3])
+    #else:
+    #    if p[2] == "<=":
+    #        p[0] =
+
+
+def p_termino(p):
+    '''
+    termino : termino1
+            | termino PLUS termino
+            | termino MINUS termino
+            | termino MULTIPLY termino
+            | termino DIVIDE termino
+    '''
+    print(p)
+
+    if(len(p) == 1):
+        p[0] = p[1]
+    else:
+        p[0] = (p[2],p[1],p[3])
+
+def p_termino1(p):
+    '''
+    termino1 : ID
+             | ID OPMATRIZ
+             | ENTERO
+             | FLOTANTE
+             | CARACTER
+             | LPAREN expresion RPAREN
+    '''
+    print(p)
+
+    if(len(p) == 1):
+        p[0] = p[1]
+    elif(len(p) == 2):
+        p[0] = (p[2],p[1])
+    elif(len(p) == 3):
+        p[0] = (p[1],p[2],p[3])
+
+#def p_empty(p):
+#    '''
+#    empty :
+#    '''
+#    p[0] = None
+
+parser = yacc.yacc()
+
+#print(parser.parse('A+B'))
