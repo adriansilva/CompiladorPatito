0801b3a47228e41e466ec0178f49035a8fd34c5a
adriansilva <adsilmen@gmail.com>
2020-05-13 01:09:01 -0500

    Se termino la logica basica del cubo semantico y del cubo de dimensiones.

--- cuboSemantico.py
+++ cuboSemantico.py
@@ -1,38 +1,89 @@
-mat = [[#INT  FLOAT  CHAR  BOOL
-       [0,     1,    -1,   -1], #INT       TYPE: '+'
-       [1,     1,    -1,   -1], #FLOAT
-       [-1,   -1,     2,   -1], #CHAR
-       [0,     1,    -1,    0], #BOOL
-       ],
+cuboTipos =   [[#INT  FLOAT  CHAR  BOOL
+               [0,     1,    -1,   -1], #INT       TYPE: '+'
+               [1,     1,    -1,   -1], #FLOAT
+               [-1,   -1,     2,   -1], #CHAR
+               [0,     1,    -1,    0], #BOOL
+               ],
 
-       [#INT  FLOAT  CHAR  BOOL
-       [0,     1,    -1,   -1], #INT       TYPE: '-'
-       [1,     1,    -1,   -1], #FLOAT
-       [-1,   -1,     2,   -1], #CHAR
-       [0,     1,    -1,    0], #BOOL
-       ],
+               [#INT  FLOAT  CHAR  BOOL
+               [0,     1,    -1,   -1], #INT       TYPE: '-'
+               [1,     1,    -1,   -1], #FLOAT
+               [-1,   -1,     2,   -1], #CHAR
+               [0,     1,    -1,    0], #BOOL
+               ],
 
-       [#INT  FLOAT  CHAR  BOOL
-       [0,     1,    -1,   -1], #INT       TYPE: '*'
-       [1,     1,    -1,   -1], #FLOAT
-       [-1,   -1,    -1,   -1], #CHAR
-       [0,     1,    -1,    0], #BOOL
-       ],
+               [#INT  FLOAT  CHAR  BOOL
+               [0,     1,    -1,    0], #INT       TYPE: '*'
+               [1,     1,    -1,    0], #FLOAT
+               [-1,   -1,    -1,   -1], #CHAR
+               [0,     1,    -1,    0], #BOOL
+               ],
 
-       [#INT  FLOAT  CHAR  BOOL
-       [0,     1,    -1,   -1], #INT       TYPE: '/'
-       [1,     1,    -1,   -1], #FLOAT
-       [-1,   -1,    -1,   -1], #CHAR
-       [-1,    1,    -1,   -1], #BOOL
-       ],
+               [#INT  FLOAT  CHAR  BOOL
+               [0,     1,    -1,   -1], #INT       TYPE: '/'
+               [1,     1,    -1,   -1], #FLOAT
+               [-1,   -1,    -1,   -1], #CHAR
+               [-1,    1,    -1,   -1], #BOOL
+               ],
 
-       [#INT  FLOAT  CHAR  BOOL
-       [3,     3,    -1,    3], #INT       TYPE: '&&'
-       [1,     1,    -1,   -1], #FLOAT
-       [-1,   -1,    -1,   -1], #CHAR
-       [-1,    1,    -1,   -1], #BOOL
-       ],
-     ]
+               [#INT  FLOAT  CHAR  BOOL
+               [3,     3,    -1,    3], #INT       TYPE: '&&', '||'
+               [3,     3,    -1,    3], #FLOAT
+               [-1,   -1,    -1,   -1], #CHAR
+               [3,     3,    -1,    3], #BOOL
+               ],
+
+               [#INT  FLOAT  CHAR  BOOL
+               [3,     3,    -1,    3], #INT       TYPE: '<=', '>=', '<>', '<', '>', '=='
+               [3,     3,    -1,    3], #FLOAT
+               [-1,   -1,     3,   -1], #CHAR
+               [3,     3,    -1,    3], #BOOL
+               ]
+             ]
+
+cuboDimensiones = [[ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '+'
+                         [1,         1,      -1], #Arreglo
+                         [2,         2,       2]  #Matriz
+                   ],
+
+                   [ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '-'
+                         [1,         1,      -1], #Arreglo
+                         [2,         2,       2]  #Matriz
+                   ],
+
+                   [ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '*'
+                         [1,         1,       1], #Arreglo
+                         [2,         2,       2]  #Matriz
+                   ],
+
+                   [ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '*'
+                         [1,         1,       1], #Arreglo
+                         [2,         2,       2]  #Matriz
+                   ],
+
+                   [ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '&&', '||'
+                         [-1,       -1,      -1], #Arreglo
+                         [-1,       -1,      -1]  #Matriz
+                   ],
+
+                   [ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '<=', '>=', '<', '>'
+                         [-1,       -1,      -1], #Arreglo
+                         [-1,       -1,      -1]  #Matriz
+                   ],
+
+                   [ #ValorÚnico  Arreglo  Matriz
+                         [0,        -1,      -1], #ValorÚnico       TYPE: '==', '<>'
+                         [-1,        1,      -1], #Arreglo
+                         [-1,       -1,       2]  #Matriz
+                   ]
+
+                  ]
 
 def typeToInt(tipo):
     switcher = {
@@ -45,18 +96,24 @@ def typeToInt(tipo):
         '*':     2,
         '/':     3,
         '&&':    4,
-        '||':    5,
-        '<=':    6,
-        '>=':    7,
-        '<>':    8,
-        '<':     9,
-        '>':     10,
-        '==':    11,
-        'UNDEF': -1,
+        '||':    4,
+        '<=':    5,
+        '>=':    5,
+        '<>':    5,
+        '<':     5,
+        '>':     5,
+        '==':    5,
+        'UNDEF': 5,
     }
-    return switcher.get(tipo,"Caracter inválido.")
+    regresa = switcher.get(tipo, "Caracter inválido.")
+    if type(regresa) is int:
+         return regresa
+    else:
+        print(regresa)
+        exit(-1)
 
-def cubo(tipo1, tipo2, operacion):
-    tipoResultante = mat[typeToInt(operacion),typeToInt(tipo1), typeToInt(tipo2)]
-    print(tipoResultante)
+def cubo(tipo1, tipo2, operacion, dimension1, dimension2):
+    tipoResultante = cuboTipos[typeToInt(operacion)][typeToInt(tipo1)][typeToInt(tipo2)]
+    dimensionResultante = cuboDimensiones[typeToInt(operacion) if (operacion != '<>' and operacion != '==') else 6][dimension1][dimension2]
+    print("tipo:",tipoResultante, " // dimension:",dimensionResultante)
     # if dimensiones == (0): regresas un
